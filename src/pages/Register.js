f4fdffad676a2ba7c8343e3262871ac89c317a24
tasuke567 <80236261+tasuke567@users.noreply.github.com>
2024-12-19 15:42:43 +0700

    ee

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -65,7 +65,7 @@ const Register = () => {
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 placeholder="Full Name"
-                value={formData.name}
+                value={formData.username}
                 onChange={handleChange}
               />
             </div>
