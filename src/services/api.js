179776d3859267b20acf4b4adff8f514b60eccdb
tasuke567 <80236261+tasuke567@users.noreply.github.com>
2024-12-19 14:13:20 +0700

    eiei

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://6763c5ae3d1a0800085f0e92--comfronteiei.netlify.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
